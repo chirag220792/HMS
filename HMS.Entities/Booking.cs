6a92ca57978076e1b22596355eb9a2dd12c1ed8c
Chirag <pokiyachirag2@gmail.com>
2020-07-28 21:25:40 +0530

    Installed Entity FrameWork and Enabled Migrations

---
+++ HMS.Entities/Booking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HMS.Entities
+{
+    public class Booking
+    {
+        public int ID { get; set; }
+        public string AccomodationID { get; set; }
+        public Accomodation Accomodation{ get; set; }
+        public DateTime FromDate { get; set; }
+        
+        /// <summary>
+        /// No of Stay Nights
+        /// </summary>
+        public int Duration{ get; set; }
+
+    }
+}
